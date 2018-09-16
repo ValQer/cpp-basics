0918553faaa694dbb33ec50260e9ed18e488dc85
ValQ <coolborwec@mail.ru>
2018-09-16 13:25:08 +0300

    first try

--- 1-linear-program/Source.cpp
+++ 1-linear-program/Source.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
-#include <math.h>
+#include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
 int main() {
+	setlocale(0, "");
+	double a, z1, z2;
+	double pi = acos(-1.0);
+	cout << "¬ведите значение a: \n";
+	cin >> a;
+	z1 = (sin(4.0 * a) * cos(2.0 * a)) / ((1.0 + cos(4.0 * a)) * (1.0 + cos(2.0 * a)));
+	z2 = 1.0 / (tan(2.0 * pi / 3.0 - a));
+	cout << z1 << " " << z2 << endl;
+	system("pause");
+	return 0;
 
 }
