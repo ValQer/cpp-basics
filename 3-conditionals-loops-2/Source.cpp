538998fe6408072b503bcb914c9058455f98ddb3
ValQ <coolborwec@mail.ru>
2018-09-23 16:56:25 +0300

    project 3 fix function body 2

--- 3-conditionals-loops-2/Source.cpp
+++ 3-conditionals-loops-2/Source.cpp
@@ -17,20 +17,19 @@ int main() {
 	cin >> dX >> E;
 
 	if (dX > 0 && X2 < -1) {
-		cout << string(45, '-') << "\n|"
+		cout << string(60, '-') << "\n|"
 			<< setw(8) << "X" << setw(7)
 			<< "|" << setw(12) << "arctg(x)"
 			<< setw(3) << "|" << setw(12)
-			<< "Iterations" << setw(3) << "|\n"
-			<< string(45, '-') << endl;
+			<< "atan(x)" << setw(3) << "|" << setw(12) << "Iterations" << setw(3) << "|\n"
+			<< string(60, '-') << endl;
 		for (double x = X1; x <= X2; x += dX) {
-			double arc1, arc2 = -3.14159265359 / 2;
-			arc1 = atan(x);
+			double arc1 = 0, arc2 = -3.14159265359 / 2;
 			for (int n = 0; n < MAxIters; n++) {
 				arc2 += pow(-1, n + 1) / ((2 * n + 1)*pow(x, 2 * n + 1));
 				if (abs(abs(arc1) - abs(arc2)) < E) {
 					cout << "|" << setw(14) << x 
-						<< "|" << setw(14) << arc2 
+						<< "|" << setw(14) << arc2 << "|" << setw(14) << atan(x)
 						<< "|" << setw(13) << n << "|\n";
 					break;
 				}
@@ -38,9 +37,10 @@ int main() {
 					cout << "ERROR!!!! Too small E!!!\n";
 					return 2;
 				}
+				arc1 = arc2;
 			}
 		}
-		cout << string(45, '-');
+		cout << string(60, '-');
 	}
 	else {
 		cout << "\nInvalid values";
