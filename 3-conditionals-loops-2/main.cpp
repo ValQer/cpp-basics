05c0c0f27377ae253dd1c18f3b13fc25c9d043f9
ValQ <coolborwec@mail.ru>
2018-09-25 00:26:07 +0300

    fix body 3 proj

--- 3-conditionals-loops-2/main.cpp
+++ 3-conditionals-loops-2/main.cpp
@@ -1,3 +1,4 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
 #include <cmath>
 #include <string>
@@ -6,42 +7,58 @@
 using namespace std;
 
 int main() {
-	double X1, X2, dX, E;
+	double X1, X2, dX, Eps;
 	long MAxIters = 10000000;
 
 	cout << fixed;
 	cout << "X must be less than -1\n";
-	cout << "Please, enter X (start) and X (end): ";
-	cin >> X1 >> X2;
-	cout << "Please, enter dX and E: ";
-	cin >> dX >> E;
+	cout << "Please, enter X (start): ";
+	cin >> X1;
+	cout << "Please, enter X (end): ";
+	cin >> X2;
+	cout << "Please, enter dX: ";
+	cin >> dX;
+	cout << "Please, enter Eps: ";
+	cin >> Eps;
 
 	if (dX > 0 && X2 < -1) {
+
+
 		cout << string(60, '-') << "\n|"
 			<< setw(8) << "X" << setw(7)
 			<< "|" << setw(12) << "arctg(x)"
 			<< setw(3) << "|" << setw(12)
 			<< "atan(x)" << setw(3) << "|" << setw(12) << "Iterations" << setw(3) << "|\n"
 			<< string(60, '-') << endl;
+
+
 		for (double x = X1; x <= X2; x += dX) {
-			double arc1 = 0, arc2 = -3.14159265359 / 2;
+
+			double arc1 = 0, arc2 = -M_PI_2;
+
 			for (int n = 0; n < MAxIters; n++) {
+
 				arc2 += pow(-1, n + 1) / ((2 * n + 1)*pow(x, 2 * n + 1));
-				if (abs(abs(arc1) - abs(arc2)) < E) {
-					cout << "|" << setw(14) << x 
+
+				if (abs(arc2 - arc1) < Eps) {
+					cout << "|" << setw(14) << x
 						<< "|" << setw(14) << arc2 << "|" << setw(14) << atan(x)
 						<< "|" << setw(13) << n << "|\n";
 					break;
 				}
+
+
 				if (MAxIters - n < 2) {
-					cout << "ERROR!!!! Too small E!!!\n";
+					cout << "ERROR!!!! Too small Eps!!!\n";
 					return 2;
 				}
+
 				arc1 = arc2;
 			}
 		}
 		cout << string(60, '-');
 	}
+
 	else {
 		cout << "\nInvalid values";
 	}
