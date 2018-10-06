c00e79b862f39f4a8449d30e4b784ca2ff478a3d
ValQ <coolborwec@mail.ru>
2018-10-06 20:15:45 +0300

    5 proj add

---
+++ 5-arrays-1d/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cmath> 
+
+using namespace std;
+
+void sortArray(double a, double b, int n, double* arr) {
+	int j = 0;
+	for (int i = 0; i < n; i++) {
+		if (a <= (int) arr[i] && (int) arr[i] <= b ) {
+			swap(arr[j], arr[i]);
+			j++;
+		}
+	}
+}
+
+int main() {
+	int n, imax = 0;
+	double max, a, b, sum = 0;
+	cout << fixed;
+
+	cout << "Enter N: ";
+	cin >> n;
+	cout << "Enter a: ";
+	cin >> a;
+	cout << "Enter b: ";
+	cin >> b;
+
+
+	if (n >= 0 && a < b) {
+		double *arr = new double [n];
+		cout << "Enter array: \n";
+		for (int i = 0; i < n; i++) {
+			cout << "[" << i + 1 << "]";
+			cin >> arr[i];
+		}
+
+		max = arr[0];
+		for (int i = 1; i < n; i++) {
+			if (abs(arr[i]) > max) {
+				max = abs(arr[i]);
+				imax = i;
+			}
+		}
+		cout << "Number of maximum element: " << imax + 1 << endl;
+
+
+		bool check = false;
+		for (int i = 0; i < n; i++) {
+			if (arr[i] > 0) 
+				check = true;
+			if (check) 
+				sum += arr[i];
+		}
+		if (check)
+			cout << "Sum of positive elements is: " << sum << endl;
+		else 
+			cout << "Array doesn't contain positive elements...." << endl;
+
+
+		sortArray(a, b, n, arr);
+		for (int i = 0; i < n; i++) {
+			cout << "[" << i + 1 << "] " << arr[i] << endl;
+		}
+	}
+	else {
+		cout << "\nInvalid value of N or (a; b) !!!";
+	}
+}
